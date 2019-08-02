8635c88f4f2144058ed1ba72a663d3ffb620660d
ethender <ethender@hotmail.com>
2019-08-02 16:53:17 +0530

    playing cards

---
+++ PlayingCard/PlayingCard/PlayingCard.swift
@@ -0,0 +1,54 @@
+//
+//  PlayingCard.swift
+//  PlayingCard
+//
+//  Created by BA on 02/08/19.
+//  Copyright © 2019 BA. All rights reserved.
+//
+
+import Foundation
+
+
+struct PlayingCard : CustomStringConvertible{
+    var description: String {return "\(rank)\(suit)"}
+    
+    var suit:Suit
+    var rank:Rank
+    
+    enum Suit: String{
+        case spades = "♠"
+        case hearts = "🧡"
+        case diamonds = "♣"
+        case clubs = "⬩"
+        
+        static var all = [Suit.spades,.hearts,.diamonds,.clubs]
+    }
+    
+    enum Rank {
+        case ace
+        case face(String)
+        case numeric(Int)
+        
+        var order: Int{
+            switch self{
+            case .ace: return 1
+            case .numeric(let pips): return pips
+            case .face(let kind) where kind=="J": return 11
+            case .face(let kind) where kind=="Q": return 12
+            case .face(let kind) where kind=="K": return 13
+            default: return 0
+            }
+        }
+        
+        static var all:[Rank]{
+            var allRanks = [Rank.ace]
+            for pips in 2...10{
+                allRanks.append(Rank.numeric(pips))
+            }
+            
+            allRanks += [Rank.face("J"),.face("Q"),.face("K")]
+            
+            return allRanks
+        }
+    }
+}
---
+++ PlayingCard/PlayingCard/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PlayingCard
+//
+//  Created by BA on 02/08/19.
+//  Copyright © 2019 BA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var deck = PlayingCardDeck()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        for _ in 1...10{
+            if let card = deck.draw(){
+                print("\(card)")
+            }
+        }
+    }
+
+
+}
+
